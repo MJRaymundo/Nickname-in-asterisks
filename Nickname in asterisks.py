64f7c2e0883926684a3d6b185595f1aa80c0b7a0
MJRaymundo <117505931+MJRaymundo@users.noreply.github.com>
2022-11-08 13:44:35 +0800

    Add files via upload

---
+++ Nickname in asterisks.py
@@ -0,0 +1,6 @@
+# Mark john M. Raymundo CoE 2-2 Data Structure and Algorithms
+print("***     ***     ****       *****")
+print("****   ****    **  **     **    **")
+print("** ** ** **   **    **   **")
+print("**  ***  **  **********   **    **")
+print("**       ** **        **   *****")
